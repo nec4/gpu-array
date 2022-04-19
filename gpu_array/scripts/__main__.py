ba6e4c4f2a2bf47368388d5701712fddc6279808
nec4 <nec4@rice.edu>
2022-04-19 04:03:57 +0200

    urwid rewrite

--- gpu_array/scripts/__main__.py
+++ gpu_array/scripts/__main__.py
@@ -7,31 +7,8 @@ from gpu_array.tui import *
 def main():
     query = GPUQuery()
     tracker = Tracker(query)
-    screen = curses_init()
-    rows, cols = screen.getmaxyx()
-    front = FrontEnd(tracker, rows, cols)
-    ch = None
-    front.refresh(screen)
-    front.run()
-    while ch != ord("q"):
-        ch = screen.getch()
-        if curses.is_term_resized(rows, cols):
-            curses.endwin()
-            screen = curses_init()
-            rows, cols = screen.getmaxyx()
-            curses.resizeterm(rows, cols)
-            front.resize(rows, cols)
-            screen.refresh()
-            continue
-        if ch == ord("p"):
-            front._swap_view()
-            front.refresh(screen)
-            continue
-        else:
-            front.refresh(screen)
-    front.stop()
-    curses.endwin()
-
+    front = FrontEnd(tracker)
+    front.start()
 
 if __name__ == "__main__":
     main()
--- gpu_array/tui.py
+++ gpu_array/tui.py
@@ -1,32 +1,8 @@
-import curses
+import urwid
 from .query import GPUQuery, Tracker
 from time import sleep
 from threading import Thread, Event
 from typing import Union
-
-
-def curses_init() -> curses.window:
-    """Function for initializing nCurses, supressing
-    getch() blocking and defining colors indicating
-    low, medium, and high values (green, yellow, and red).
-
-    Returns
-    -------
-    screen:
-        curses.window instance representing the stdscr
-    """
-    screen = curses.initscr()
-    curses.noecho()
-    curses.curs_set(0)
-    curses.start_color()
-    curses.use_default_colors()
-    curses.init_pair(1, 2, -1)  # low
-    curses.init_pair(2, 3, -1)  # medium
-    curses.init_pair(3, 1, -1)  # high
-    screen.keypad(1)
-    screen.idcok(False)
-    screen.nodelay(1)
-    return screen
 
 
 class PollThread(Thread):
@@ -57,19 +33,9 @@ class PollThread(Thread):
 
 
 class FrontEnd(object):
-    """TUI for GPU information
 
-    Parameters
-    ----------
-    tracker:
-        Tracker instance for requesting, parsing and storing
-        GPU information.
-    rows:
-        Integer number of rows available in the current terminal
-    cols:
-        Integer number of columns available in the current terminal
-    """
-
+    box_padding = 1
+    card_width = 35
     overwatch_labels = {
         "memory": "Mem ",
         "temperature": "Tmp ",
@@ -78,102 +44,113 @@ class FrontEnd(object):
     }
     process_labels = {"pid": "PID  ", "memory": "Mem  ", "name": "Name "}
 
-    card_buffer_y = 10
-    card_buffer_x = 10
+    palette = [
+        ("low", "dark green", ""),
+        ("medium", "brown", ""),
+        ("high", "dark red", ""),
+        ("rev_inc", "", "light gray"),
+        ("rev", "", "black"),
+    ]
 
-    def __init__(self, tracker: Tracker, rows: int, cols: int):
-        self.tracker = tracker
-        self.poll_thread = None
-        self.indent = 4
+    def __init__(self, tracker):
         self.current_view = "overwatch"
-        self.resize(rows, cols)
+        self.top = None
+        self.tracker = tracker
+        self._initialize_grid()
+        self.loop = urwid.MainLoop(
+            self.top, palette=FrontEnd.palette, unhandled_input=self.keypress
+        )
+        self.loop.set_alarm_in(sec=1, callback=self._draw)
 
-    def resize(self, rows: int, cols: int):
-        """Method for adjusting the TUI in response to
-        a terminal resize/SIGWINCH
-
-        Parameters
-        ----------
-                rows:
-                        Integer number of rows available in the current terminal
-                cols:
-                        Integer number of columns available in the current terminal
-        """
-        self.rows, self.cols = rows, cols
-        self._initialize_gpu_windows()
-
-    def run(self):
-        """Method to start the polling thread"""
-        self.poll_thread = PollThread(self.tracker)
-        self.poll_thread.start()
-
-    def stop(self):
-        """Method to stop the polling thread"""
-        self.poll_thread.join()
-
-    def clear_array(self):
-        """Clears all windows and redraws borders"""
-        for win_row in self.window_array:
-            for window in win_row:
-                window.clear()
-                window.border(*[0 for _ in range(8)])
-
-    def _swap_view(self):
+    def _switch_view(self):
         """Swaps current card view"""
         if self.current_view == "overwatch":
             self.current_view = "process"
-            self.clear_array()
+            self._initialize_grid()
             return None
         if self.current_view == "process":
             self.current_view = "overwatch"
-            self.clear_array()
+            self._initialize_grid()
             return None
 
-    def draw(self):
-        """Depending on the current view, runs drawing routines"""
+    def start(self):
+        self.poll_thread = PollThread(self.tracker)
+        self.poll_thread.start()
+        self.loop.run()
+
+    def stop(self):
+        self.poll_thread.join()
+        raise urwid.ExitMainLoop()
+
+    def keypress(self, key):
+        if key in ("q", "Q"):
+            self.stop()
+        if key in ("p"):
+            self._switch_view()
+
+    @staticmethod
+    def _initialize_gauge_card():
+        pile = []
+        pile.append(urwid.Text(""))
+        for _ in range(4):
+            pile.append(
+                urwid.Padding(
+                    urwid.Text(""),
+                    left=2
+                )
+            )
+            pile.append(
+                urwid.Padding(
+                    urwid.ProgressBar("rev", "rev_inc", current=0),
+                    width=("relative", 90),
+                    align="center",
+                )
+            )
+        box = urwid.LineBox(urwid.Pile(pile))
+        return box
+
+    @staticmethod
+    def _initialize_proc_card():
+        pile = []
+        pile.append(urwid.Text(""))
+        pile.append(
+            urwid.Padding(
+                urwid.Text(""),
+                left=2
+            )
+        )
+        box = urwid.LineBox(urwid.Pile(pile))
+        return box
+
+    def _update_proc_pile(self, name_str, proc_strs):
+        pile = []
+        pile.append(urwid.Text(name_str))
+        for p in enumerate(proc_strs):
+            pile.append(
+                urwid.Padding(
+                    urwid.Text(p),
+                    left=2
+                )
+            )
+        box = urwid.LineBox(urwid.Pile(pile))
+        return box
+
+    def _initialize_grid(self):
         if self.current_view == "overwatch":
-            self._draw_overwatch()
+            card_list = [FrontEnd._initialize_gauge_card() for _ in range(self.tracker.num_gpus)]
         if self.current_view == "process":
-            self._draw_process()
-
-    def _initialize_gpu_windows(self):
-        """Method for setting up the window array to encompass
-        information for individual GPUS
-        """
-        # screen will broken into n squares
-        # where n is the nearest power of two
-        powers = [i ** 2 for i in range(1, 4)]
-        diffs = [i - self.tracker.num_gpus for i in powers]
-        _, idx = min((val, idx) for (idx, val) in enumerate(diffs) if val >= 0)
-        num_windows = powers[idx]
-        win_rows = idx + 1
-        self.win_rows = win_rows
-        win_size_y = (self.rows // win_rows) - 1
-        win_size_x = (self.cols // win_rows) - 1
-
-        if win_size_y <= FrontEnd.card_buffer_y:
-            curses.endwin()
-            if self.poll_thread != None:
-                self.stop()
-            raise RuntimeError("Terminal too small in Y!")
-        if win_size_x <= FrontEnd.card_buffer_x:
-            curses.endwin()
-            if self.poll_thread != None:
-                self.stop()
-            raise RuntimeError("Terminal too small in X!")
-
-        self.window_array = [
-            [
-                curses.newwin(win_size_y, win_size_x, i * win_size_y, j * win_size_x)
-                for j in range(win_rows)
-            ]
-            for i in range(win_rows)
-        ]
-
-        for i in range(len(self.window_array)):
-            for j in range(len(self.window_array)):
-                self.window_array[i][j].border(*[0 for _ in range(8)])
-                self.window_array[i][j].scrollok(1)
+            card_list = [FrontEnd._initialize_proc_card() for _ in range(self.tracker.num_gpus)]
+        self.grid = urwid.GridFlow(
+            card_list,
+            FrontEnd.card_width,
+            FrontEnd.box_padding,
+            FrontEnd.box_padding,
+            "center",
+        )
+        if self.top == None:
+            self.top = urwid.Filler(self.grid)
+        else:
+            self.top.original_widget = self.grid
 
     @staticmethod
     def _determine_color(val: int) -> int:
@@ -191,51 +168,61 @@ class FrontEnd(object):
             curses window attribute
         """
         if val < 33:
-            color = curses.color_pair(1)
+            color = "low"
         elif val > 33 and val < 66:
-            color = curses.color_pair(2)
+            color = "medium"
         else:
-            color = curses.color_pair(3)
+            color = "high"
         return color
+
+    def _draw(self, *args):
+        """Depending on the current view, runs drawing routines"""
+        if self.current_view == "overwatch":
+            self._draw_overwatch()
+        if self.current_view == "process":
+            self._draw_process()
+        self.loop.set_alarm_in(1, self._draw)
 
     def _draw_process(self):
         """Method for drawing process information to each GPU window"""
         all_gpu_props = self.tracker.props_buffer
         if all_gpu_props != None:
             gpu_ids = sorted(all_gpu_props.keys())
+            card_list = []
             for gpu_id in gpu_ids:
-                row = gpu_id // self.win_rows
-                col = gpu_id % self.win_rows
-                window = self.window_array[row][col]
+                name_string = all_gpu_props[gpu_id]["name"]
                 processes = all_gpu_props[gpu_id]["processes"].keys()
-                try:
-                    window.addstr(
-                        1,
-                        1,
-                        " {}: {} ".format(gpu_id, all_gpu_props[gpu_id]["name"]),
+                proc_strs = []
+                for i, process in enumerate(processes):
+                    proc = all_gpu_props[gpu_id]["processes"][process]
+                    proc_string = "{}: {} {} {} {}".format(
+                        proc["user"],
+                        process,
+                        proc["name"],
+                        proc["lifetime"],
+                        proc["mem"],
                     )
-                    for i, process in enumerate(processes):
-                        proc = all_gpu_props[gpu_id]["processes"][process]
-                        proc_string = "{}: {} {} {} {}".format(
-                            proc["user"],
-                            process,
-                            proc["name"],
-                            proc["lifetime"],
-                            proc["mem"],
-                        )
-                        window.addstr(2 + i, self.indent, proc_string)
-                except:
-                    window.addstr(1, 1, "Expand terminal - currently too small ...")
+                    proc_strs.append(proc_string)
+                card_list.append(self._update_proc_pile(name_string, proc_strs))
+            self.grid = urwid.GridFlow(
+                card_list,
+                FrontEnd.card_width,
+                FrontEnd.box_padding,
+                FrontEnd.box_padding,
+                "center",
+            )
+            if self.top == None:
+                self.top = urwid.Filler(self.grid)
+            else:
+                self.top.original_widget = self.grid
 
-    def _draw_overwatch(self):
-        """Method for drawing GPU stats to each GPU window"""
+
+    def _draw_overwatch(self, *args):
         all_gpu_props = self.tracker.props_buffer
         if all_gpu_props != None:
             gpu_ids = sorted(all_gpu_props.keys())
             for gpu_id in gpu_ids:
-                row = gpu_id // self.win_rows
-                col = gpu_id % self.win_rows
-
+                name_string = all_gpu_props[gpu_id]["name"]
                 mem_string = "{}{}/{} MiB".format(
                     FrontEnd.overwatch_labels["memory"],
                     all_gpu_props[gpu_id]["used_mem"],
@@ -278,84 +265,23 @@ class FrontEnd(object):
                 )
                 fan_frac = all_gpu_props[gpu_id]["fan"]
 
-                fan_color = FrontEnd._determine_color(fan_frac)
-                mem_color = FrontEnd._determine_color(mem_frac)
-                power_color = FrontEnd._determine_color(power_frac)
-                temp_color = FrontEnd._determine_color(temp_frac)
-
-                window = self.window_array[row][col]
-                window.addstr(
-                    1,
-                    1,
-                    " {}: {} ({:03}%)".format(
-                        gpu_id,
-                        all_gpu_props[gpu_id]["name"],
-                        all_gpu_props[gpu_id]["utilization"],
-                    ),
+                card_contents = self.top.original_widget.contents[gpu_id][
+                    0
+                ].original_widget.contents
+                card_contents[0][0].set_text(name_string)
+                card_contents[1][0].original_widget.set_text(
+                    (self._determine_color(mem_frac), mem_string)
                 )
-                window.addstr(
-                    2,
-                    self.indent,
-                    mem_string,
-                    mem_color,
+                card_contents[2][0].original_widget.set_completion(mem_frac)
+                card_contents[3][0].original_widget.set_text(
+                    (self._determine_color(fan_frac), fan_string)
                 )
-                self.draw_bar(window, 3, self.indent, mem_frac, mem_color)
-
-                window.addstr(
-                    4,
-                    self.indent,
-                    fan_string,
-                    fan_color,
+                card_contents[4][0].original_widget.set_completion(fan_frac)
+                card_contents[5][0].original_widget.set_text(
+                    (self._determine_color(temp_frac), temp_string)
                 )
-                self.draw_bar(window, 5, self.indent, fan_frac, fan_color)
-
-                window.addstr(6, self.indent, temp_string, temp_color)
-                self.draw_bar(window, 7, self.indent, temp_frac, temp_color)
-
-                window.addstr(8, self.indent, power_string, power_color)
-                self.draw_bar(window, 9, self.indent, power_frac, power_color)
-
-    def draw_bar(self, window: curses.window, y: int, x: int, percent: int, color: int):
-        """Helper method for drawing percentage gauges.
-
-        Parameters
-        ----------
-        window:
-            curses.window instance in which the bar will be drawn
-        y:
-            row within the supplied window on which the bar is drawn
-        x:
-            column from the supplied window on which the bar starts
-        percent:
-            fraction of the bar to fill
-        color:
-            curses window attribute describing the color of the bar
-        """
-        _, size_x = window.getmaxyx()
-        length = size_x - x - 4
-        filled = int(length * (percent / 100.0))
-        unfilled = length - filled
-        window.addstr(y, x, "[")
-        window.addstr(
-            y, x + 1, "".join([" " for i in range(filled)]), curses.A_REVERSE | color
-        )
-        window.addstr(y, x + filled, "".join([" " for i in range(unfilled)]))
-        window.addstr(y, x + filled + unfilled, "]")
-
-    def refresh_array(self):
-        """Method for sequentially refreshing each GPU window"""
-        for i in range(len(self.window_array)):
-            for j in range(len(self.window_array)):
-                self.window_array[i][j].refresh()
-
-    def refresh(self, screen: curses.window):
-        """Wrapper method for refreshing the entire TUI
-
-        Parameters
-        ----------
-        screen:
-            curses.window instance representing the stdscr
-        """
-        self.draw()
-        screen.refresh()
-        self.refresh_array()
+                card_contents[6][0].original_widget.set_completion(temp_frac)
+                card_contents[7][0].original_widget.set_text(
+                    (self._determine_color(power_frac), power_string)
+                )
+                card_contents[8][0].original_widget.set_completion(power_frac)
